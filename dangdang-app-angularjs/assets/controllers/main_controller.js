1c32496c49e0d75a83861764584b8e3f7264c8a9
13017601815 <1215707997@qq.com>
2016-10-19 16:18:46 +0800

    提交

--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,5 +1,9 @@
 app.controller('mainController',['$scope','commonService',function($scope,c_service){
   c_service.getData('ertong',function(res){
     console.dir(res);
-  })
+  });
+  $scope.selectedIndex = 0;
+  $scope.linkTo = function(index){
+    $scope.selectedIndex = index;
+  }
 }])
