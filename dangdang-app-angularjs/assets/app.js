b1dfb85e36433690fa34bdc87005f93864d3077b
13017601815 <1215707997@qq.com>
2016-10-19 16:59:17 +0800

    书籍页面

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -1,1 +1,24 @@
 var app = angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/',{
+          templateUrl:'./assets/tpl/index.html'  //./表示在当前目录
+        })
+
+        .when('/blog', {
+          templateUrl:'./assets/tpl/blog.html',
+          controller:''
+        })
+        .when('/book',{
+          templateUrl:'./assets/tpl/book.html',
+          controller:'bookController'
+        })
+        .when('/about_me',{
+          templateUrl:'./assets/tpl/about_me.html',
+          controller:''
+        })
+        .otherwise({
+          redirectTo:'/'
+        })
+}])
