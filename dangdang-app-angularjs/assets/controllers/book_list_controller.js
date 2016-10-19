0fe4cc2a472615cf00d2b71ddb39448d64c3cf7d
13017601815 <1215707997@qq.com>
2016-10-19 22:17:09 +0800

    w

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -3,6 +3,7 @@ app.controller('bookListController',
 function($scope,c_service,$routeParams){
   $scope.data = [];
   var id = $routeParams.id;
+  console.log($routeParams.id);
   c_service.getData(id,function(res){
     console.dir(res);
     $scope.data = res.data.data;
