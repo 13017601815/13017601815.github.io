eb20e9fe954dfa29aa74763a2d96f3c453f159c7
13017601815 <1215707997@qq.com>
2016-10-19 15:47:08 +0800

    提交

--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,4 +1,4 @@
-app.controller('mainController',['$scope','commonService',function($scope,commonService){
+app.controller('mainController',['$scope','commonService',function($scope,commonService,c_service){
   c_service.getData('ertong',function(res){
     console.dir(res);
   })
