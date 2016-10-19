1414d5a8e8d34f4748b07b425af040e70c92fb37
13017601815 <1215707997@qq.com>
2016-10-19 20:32:02 +0800

    js

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,7 +1,6 @@
 app.controller('bookListController',['$scope','commonService',function($scope,c_service){
   $scope.data = [];
-  var type = $scope.bookTypes.id;
-  c_service.getData(type,function(res){
+  c_service.getData(function(res){
     $scope.data = res.data.data;
   })
 }])
