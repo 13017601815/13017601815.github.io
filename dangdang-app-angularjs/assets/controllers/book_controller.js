e4787d4149ed9f03715981075bf3ce0f523dcb85
13017601815 <1215707997@qq.com>
2016-10-19 20:42:42 +0800

    image

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,9 +1,9 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes = [
-    {id:'ertong',name:'儿童',img:'',description:''},
-    {id:'lishi',name:'历史',img:'',description:''},
-    {id:'dongman',name:'动漫',img:'',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'',description:''}
+    {id:'ertong',name:'儿童',img:'http://pic.58pic.com/10/81/58/68bOOOPIC49.jpg',description:''},
+    {id:'lishi',name:'历史',img:'http://pic26.photophoto.cn/20130118/0022005592182146_b.jpg',description:''},
+    {id:'dongman',name:'动漫',img:'http://www.yooyoo360.com/photo/2009-1-1/20090112132729896.jpg',description:''},
+    {id:'qingchunwenxue',name:'青春文学',img:'http://pic.58pic.com/58pic/15/28/04/60S58PICzNZ_1024.jpg',description:''}
   ]
 
 }])
