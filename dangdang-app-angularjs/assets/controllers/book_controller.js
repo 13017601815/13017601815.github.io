880c618c212c9941109321556171ee924ffb38e7
13017601815 <1215707997@qq.com>
2016-10-19 17:10:23 +0800

    image

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,9 +1,9 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes = [
-    {id:'ertong',name:'儿童',img:'',description:''},
-    {id:'lishi',name:'历史',img:'',description:''},
-    {id:'dongman',name:'动漫',img:'',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'',description:''}
+    {id:'ertong',name:'儿童',img:'童书.png',description:''},
+    {id:'lishi',name:'历史',img:'历史.png',description:''},
+    {id:'dongman',name:'动漫',img:'小说.png',description:''},
+    {id:'qingchunwenxue',name:'青春文学',img:'文学.png',description:''}
   ]
-  
+
 }])
