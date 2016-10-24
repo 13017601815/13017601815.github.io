d9ec8add372f076db26db96c6b89ebae52b2357a
13017601815 <1215707997@qq.com>
2016-10-24 15:07:57 +0800

    流量统计

---
+++ baiduMap/app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('./public'));
+
+app.listen(3000,function () {
+    console.log('服务器运行于3000端口');
+})
---
+++ baiduMap/readme.md
@@ -0,0 +1,4 @@
+####百度地图api
+
+1.地址:官网地址http://lbsyun.baidu.com/
+2.案例地址:http://developer.baidu.com/map/jsdemo.htm#a1_2
